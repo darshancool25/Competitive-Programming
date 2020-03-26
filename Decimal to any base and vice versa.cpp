2ee4310f76afbc8ab689c0d37de702da864165af
darshancool25 <lokhande.darshan25@gmail.com>
2020-03-26 18:28:02 +0530

    Create Decimal to any base and vice versa.cpp
    Base conversion functions for C++

---
+++ Decimal to any base and vice versa.cpp
@@ -0,0 +1,63 @@
+Decimal to Any Base :
+
+char reVal(int num)
+{
+	if (num >= 0 && num <= 9)
+		return (char)(num + '0');
+	else
+		return (char)(num - 10 + 'A');
+}
+
+string fromDeci(int inputNum, int base)
+{
+	string res = "";
+	while (inputNum > 0)
+	{
+		res += reVal(inputNum % base);
+		inputNum /= base;
+	}
+	reverse(res.begin(), res.end());
+
+	return res;
+}
+
+----------------------------------------------------------------------------------
+Any Base to Decimal :
+
+int val(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (int)c - '0';
+	else
+		return (int)c - 'A' + 10;
+}
+int toDeci(string str, int base)
+{
+	int len = str.length(), power = 1, num = 0, i;
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (val(str[i]) >= base)
+		{
+			cout << "INVALID NUMBER!";
+			return -1;
+		}
+		num += val(str[i]) * power;
+		power = power * base;
+	}
+	return num;
+}
+
+--------------------------------------------------------------------------------
+Convert decimal to Binary String of same lengths :
+
+string decToBinary(int n)
+{
+	// Size of an integer is assumed to be 32 bits
+	string s;
+	for (int i = 31; i >= 0; i--) {
+		int k = n >> i;
+		if (k & 1)	s += '1';
+		else	s += '0';
+	}
+	return s;
+}
