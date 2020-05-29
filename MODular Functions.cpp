9eb030178e0194c9f63279af312d29d0dc2eafe4
darshancool25 <lokhande.darshan25@gmail.com>
2020-05-30 01:45:59 +0530

    Modular functions added

---
+++ MODular Functions.cpp
@@ -0,0 +1,5 @@
+ll add(ll x, ll y) {ll res = x + y; return (res >= MOD ? res - MOD : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= MOD ? res % MOD : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + MOD : res);}
+ll power(ll x, ll y) {if (y < 0) return 1; ll res = 1; x %= MOD; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, MOD - 2);}
