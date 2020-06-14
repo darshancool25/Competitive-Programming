76d658d48c005fd98fe754c4ff6d6c285ac27e45
darshancool25 <lokhande.darshan25@gmail.com>
2020-06-14 13:03:28 +0530

    Update Difference Array.cpp

--- Difference Array.cpp
+++ Difference Array.cpp
@@ -4,24 +4,27 @@ int diff[10005], cover[10003];
 
 void update(int l, int r, int x)
 {
-	diff[l] += x;
-	diff[r + 1] -= x;
+    diff[l] += x;
+    diff[r + 1] -= x;
 }
 
 void work(int l, int r)
 {
-	cover[l] = diff[l];
-	for (int i = l + 1; i <= r; i++)
-		cover[i] = diff[i] + cover[i - 1];
+    cover[l] = diff[l];
+    for (int i = l + 1; i <= r; i++)
+        cover[i] = diff[i] + cover[i - 1];
 }
 
 void clean(int l, int r)
 {
-	for (int i = l; i <= r; ++i)
-		cover[i] = diff[i] = 0;
+    for (int i = l; i <= r; ++i)
+        cover[i] = diff[i] = 0;
 }
 
 //Note : Dont forget to clean arrays before use;
+
+[CF BLOG]
+https://codeforces.com/blog/entry/78762
 
 [Easy]
 https://codeforces.com/contest/1343/problem/D
@@ -30,3 +33,4 @@ soln - https://codeforces.com/contest/1343/submission/77688710
 [Medium]
 Hackerrank - Cloudy Day Problem
 https://www.hackerrank.com/challenges/cloudy-day/problem
+
